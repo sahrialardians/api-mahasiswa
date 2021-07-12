28f8d3d59e6a179b572fda959a48516cb35597f4
Sahrial Ardians <sahrialardians@gmail.com>
2021-07-12 09:15:58 +0700

    setup server nodejs

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+// parse aplication/json
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
+
+app.listen(port, () => {
+  console.log(`Server app listening at http://localhost:${port}`)
+});
