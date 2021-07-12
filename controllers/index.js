6b76d820ac78a94930d9117d9c6ea836f6fc8432
Sahrial Ardians <sahrialardians@gmail.com>
2021-07-12 10:06:43 +0700

    create endpoint get mahasiswa

--- controllers/index.js
+++ controllers/index.js
@@ -6,3 +6,14 @@ const connection = require('../connection');
 exports.index = ((req, res) => {
     response.ok("Alhamdulillah, rest-api is running!", res)
 });
+
+// Get data mahasiswa
+exports.getAllData = ((req, res, message) => {
+    connection.query('SELECT * FROM mahasiswa', (error, rows, cols) => {
+        if(error){
+            connection.log(error)
+        }else{
+            response.ok(rows, res, message)
+        }
+    })
+})
