8763d7092dcc68b6b5d7554c7ca05b5c541d6377
Sahrial Ardians <sahrialardians@gmail.com>
2021-07-12 09:44:19 +0700

    seting route at server.js

--- server.js
+++ server.js
@@ -2,14 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
+const routes = require('./routes');
 
 // parse aplication/json
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-});
+routes(app);
 
 app.listen(port, () => {
   console.log(`Server app listening at http://localhost:${port}`)
