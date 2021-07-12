297748046a184f92f37e9aa5e92cac962981ccc4
Sahrial Ardians <sahrialardians@gmail.com>
2021-07-12 09:37:21 +0700

    create controller js

---
+++ controllers/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const response = require('../res');
+const connection = require('../connection');
+
+exports.index = ((req, res) => {
+    response.ok("Alhamdulillah, rest-api is running!")
+});
