48f36f818c052394a23151bd14bbe30c9f1ed3f6
Sahrial Ardians <sahrialardians@gmail.com>
2021-07-13 14:23:30 +0700

    register endpoint test-auth with verification to routes

--- middleware/index.js
+++ middleware/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const auth = require('./auth');
+const verification = require('./authorization');
 const router = express.Router();
 
 // router
 router.post('/register', auth.register);
 router.post('/login', auth.login);
+
+// endpoint with authorization
+router.get('/test-auth', verification("USER"), auth.notAkses);
 
 module.exports = router;
 
