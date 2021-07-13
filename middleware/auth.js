b08f5fbe86cb1aec31cdf1fbaa018994c970d341
Sahrial Ardians <sahrialardians@gmail.com>
2021-07-13 09:51:51 +0700

    create modolu login user

--- middleware/auth.js
+++ middleware/auth.js
@@ -44,3 +44,58 @@ exports.register = ((req, res) => {
         }
     }))
 });
+
+// login user controller
+exports.login = ((req, res) => {
+    let request = {
+        email: req.body.email,
+        password: req.body.password
+    }
+
+    let query = "SELECT * FROM ??=? WHERE ??=?";
+    let table = ["users", "email", request.email, "password", md5(request.email)];
+
+    query = mysql.format(query, table);
+    connection.query(query, ((error, rows) => {
+        if(error){
+            console.log(error);
+        }else{
+            if (rows == 1) {
+                let token = jwt.sign({rows}, config.secret, {
+                    expiresIn: 86400 // expired 24 jam
+                });
+                user_id = rows[0].id;
+
+                let data = {
+                    user_id: user_id,
+                    access_token: token,
+                    ip_address: ip.address()
+                }
+
+                let query = "INSERT INTO ?? SET ?";
+                let table = ["access_tokens"];
+
+                query = mysql.format(query, table);
+                connection.query(query, data, ((error, rows) => {
+                    if (error) {
+                        console.log(error);
+                    } else {
+                         res.json({
+                             status: 200,
+                             message: "Access token generated!",
+                             data: {
+                                 token: token,
+                                 user: data.user_id
+                             }
+                         });
+                    }
+                }));
+            } else {
+                 res.json({
+                     status: 422,
+                     message: "Email or Password invalid!",
+                 });
+            }
+        }
+    }));
+})
